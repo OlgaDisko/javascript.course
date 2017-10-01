ba44b5eb2a931a5ce1cdbf39048dae205a893523
Olga Disko <ola_disko@mail.ru>
2017-10-02 01:26:02 +0300

    Add files via upload

---
+++ taylorforarcsin.js
@@ -0,0 +1,20 @@
+var n=prompt();
+var k=prompt();
+var a,b,c,d,sum;
+for (i=1;i<=2*n;i++){
+	b*=i;
+}
+for(i=1;i<=4*n;i++){
+	c*=i;
+}
+for(i=1;i<=n;i++){
+	d*=i;
+}
+for(i=1;i<=2*n+1;i++){
+	a*=a
+}
+sum=1-a*b/(c*d*(2*n+1));
+for(i=0;i<=k;i++){
+	sum+=sum
+}
+alert(sum);
