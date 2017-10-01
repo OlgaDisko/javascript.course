b19347c4ac17628e83780981e91d76cdd7238fb1
Olga Disko <ola_disko@mail.ru>
2017-10-02 01:39:28 +0300

    Update taylorforarcsin.js

--- taylorforarcsin.js
+++ taylorforarcsin.js
@@ -1,9 +1,6 @@
 var x=Number(prompt());
 var n=Number(prompt());
 var sum;
-for(var i = 0; i <= n; i++){
-		sum += (factorial(2*i) / (Math.pow(4, i) * Math.pow(factorial(i), 2) * (2*i+1))) * Math.pow(x, 2*i+1);
-	}
 function factorial(a){
 	if (a==0 || a==1) {
 		return 1;
@@ -12,5 +9,8 @@ function factorial(a){
 		return a*factorial(a-1);
 	} 
 }
+for(var i = 0; i <= n; i++){
+		sum += (factorial(2*i) / (Math.pow(4, i) * Math.pow(factorial(i), 2) * (2*i+1))) * Math.pow(x, 2*i+1);
+	}
 
 alert("Arcsin equal:"+sum);
