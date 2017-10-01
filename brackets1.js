40bf397e085436e820c763028f4205569c0dcad1
dolgho <ola_disko@mail.ru>
2017-10-01 19:56:09 +0300

    Update brackets1.js

--- brackets1.js
+++ brackets1.js
@@ -1,11 +1,9 @@
 var a=prompt();
 var n=0,i;
-var b="(";
-var c=")";
 for(i=0;i<=a.length;i++){
-	if(a.charAt(i)==b){
+	if(a.charAt(i)=="("){
 		n+=1;
-	}else if(a.charAt(i)==c){
+	}else if(a.charAt(i)==")"){
 		n-=1;
 	}
 }
