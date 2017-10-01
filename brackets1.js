13176f18aff4ffdd846c665d44681c82b64627d0
dolgho <ola_disko@mail.ru>
2017-10-01 22:18:19 +0300

    Update brackets1.js

--- brackets1.js
+++ brackets1.js
@@ -5,11 +5,14 @@ for(i=0;i<=a.length;i++){
 		n+=1;
 	}else if(a.charAt(i)==")"){
 		n-=1;
+	if(n==-1){
+		alert("The order is incorrect.");
+	}
 	}
 }
 if (n==0){
 	alert("The order is correct.");
 }
 else{
-	alert("The order is incorrect");
+	alert("The order is incorrect.");
 }
