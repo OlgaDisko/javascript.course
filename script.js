5065c520491168bf72150052ef348bc0a4182fbd
Disko Olga Sergeevna <dolgho.d@gmail.com>
2017-10-02 19:07:56 +0300

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+alert();
+/*a=prompt();
+b=prompt();
+c=prompt();
+Number(a);
+Number(b);
+Number(c);
+var res;
+if (a>b && a>c)
+{res=a}
+else if(b>c && b>a)
+{res=b} 
+else res=c
+alert(res);*/
+var a=prompt();
+if (Number(a))
+	{alert("True")}
+else alert("False")
